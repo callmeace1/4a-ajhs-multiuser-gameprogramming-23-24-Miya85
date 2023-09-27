14ca4bc7dd7138fea7b9e51ef1488a772ef04dc5
Jamiya B <89568531+Miya85@users.noreply.github.com>
2023-09-27 12:42:32 -0400

    iklu.jhiu;n

---
+++ multiUserGameProgramming/computer_science_exercises/00_helloWorld/helloWorld.cs
@@ -0,0 +1,31 @@
+// Jamiya Bates, Hello World, v0.0
+using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] argos)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Data Types
+            // Integers -- +/- whole numbers
+            // Float -- +/- decimal numbers
+            // String -- collection of letters, numbers, characters inside * *
+            // Boolean -- True or False values.
+
+            // DATA TYPES MUST BE DECLARED AND CANNOT CHANGE!
+            int myAge = 17;
+            int myHeight = 5 feet and 3 inches;
+
+            float myGPA = 3.0
+
+            string myName = "Jamiya Bates";
+            bool showeredToday = True;
+            
+
+
+        }
+    }
+}
